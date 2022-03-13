b441e5836c63ba8f1264998fbf6dc9d6a5e79db8
mikemasatsugu <mike.masatsugu@gmail.com>
2022-03-13 12:20:15 -1000

    Modified LC680

--- JS/680-valid-palindrome-II.js
+++ JS/680-valid-palindrome-II.js
@@ -43,7 +43,28 @@ s consists of lowercase English letters.
       return true;
   }
   
-  let left = 0;
+  let left = 0;/**
+  * @param {number[]} nums1
+  * @param {number} m
+  * @param {number[]} nums2
+  * @param {number} n
+  * @return {void} Do not return anything, modify nums1 in-place instead.
+  */
+ var merge = function(nums1, m, nums2, n) {
+     let insertPositionOfNums1 = m + n - 1;
+   m -= 1;
+   n -= 1;
+   while (n >= 0) {
+     if (nums1[m] > nums2[n]) {
+       nums1[insertPositionOfNums1] = nums1[m];
+       m -= 1;
+     } else {
+       nums1[insertPositionOfNums1] = nums2[n];
+       n -= 1;
+     }
+     insertPositionOfNums1 -= 1;
+   }
+ };
   let right = s.length - 1;
   
   while (left < right) {

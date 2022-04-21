b73a24cb10ad735ce623820f811cf88a04e19a49
mikemasatsugu <mike.masatsugu@gmail.com>
2022-04-21 11:41:35 -1000

    Finished LC125 Python

---
+++ Python/125-valid-palindrome.py
@@ -0,0 +1,49 @@
+'''
+https://leetcode.com/problems/valid-palindrome/
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 10^5
+s consists only of printable ASCII characters.
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        formatted_string = re.sub('[^a-zA-Z0-9]', '', s).lower()
+        left = 0
+        right = len(formatted_string) - 1
+        
+        while left <= right:
+            if formatted_string[left] != formatted_string[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
+
+
+# Intuition: remove non-alphanumeric characters and make case uniform. use two pointers to move from right and left to compare strings.  If they don't match, return False.  If we can traverse through all characters without returning False, return True

9c45b997bc8d8830a26ea948d2a8036e4bbd93a6
mikemasatsugu <mike.masatsugu@gmail.com>
2022-04-27 08:59:03 -1000

    LC21 Python Finished

---
+++ Python/21-merge-two-sorted-lists.py
@@ -0,0 +1,59 @@
+'''
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+            
+        tail.next = list1 or list2
+            
+        return dummy.next
+
+
+# Intuition: Use a dummy node to hold new linked list, and a pointer to add to it.  Set pointer to equal empty node, and use a while loop to iterate while list1 and list2 have values.  Compare the current values and set pointer's next to the node with the smaller value, then move that list's pointer over one.  pointer should traverse to pointer.next so we assign in order.  Return dummy node's next (since first node has no value)

46f31bcd8558df30e97b145f146f0ef6a8b198dc
mikemasatsugu <mike.masatsugu@gmail.com>
2022-04-29 12:59:54 -1000

    LC412 Python Finished

---
+++ Python/412-fizz-buzz.py
@@ -0,0 +1,57 @@
+'''
+https://leetcode.com/problems/fizz-buzz/
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 10^4
+'''
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        cache = []
+        
+        for num in range(1, n + 1):
+            temp = ''
+            if num % 3 == 0:
+                temp += 'Fizz'
+            if num % 5 == 0:
+                temp += 'Buzz'
+            
+            if len(temp) == 0:
+                temp = str(num)
+                
+            cache.append(temp)
+            
+        return cache
+            
+'''
+Runtime: 37 ms, faster than 96.35% of Python3 online submissions for Fizz Buzz.
+Memory Usage: 15.2 MB, less than 18.11% of Python3 online submissions for Fizz Buzz.
+'''
+
+
+
+# Intuition: String concatenation: iterate through range 1,n + 1 and build a temp string.  Concat given strings based on divisor, and if nothing is appended, temp is stringified number.  Append temp string to cache array and return cache when finished iterating.
